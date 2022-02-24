bc4164ad9bdd668fa703fcf8b004a555a40142b2
Brooke Strand <brooke.strand007@gmail.com>
2022-02-23 21:55:36 -0500

    commit tree 410f946b0c907db3c8aab01bcc0371eab51fa153

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your Circle class here
+class Circle {
+    constructor() {
+        this.radius = radius;
+    }
+    get diameter() {
+        return this.radius*2;
+    }
+    set diameter(radius) {
+        this.radius = radius;
+    }
+    get circumference() {
+        return this.radius * Math.PI;
+    }
+    set circumference(radius) {
+        this.radius = radius;
+    }
+    get area() {
+        return Math.PI * (this.radius * this.radius);
+    }
+    set area(radius) {
+        this.radius = radius;
+    }
+}
